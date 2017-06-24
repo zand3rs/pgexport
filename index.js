62d697829a6e4118e5e0d7611c24fad1b50617a5
zander <zand3rs@gmail.com>
2017-06-24 17:59:46 +0800

    added where clause support

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ var args = process.argv.slice(2);
 program
   .version(package.version, "-v, --version")
   .option("--connect <connect>", "Db connection string")
-  .option("--from <from>", "Source table or sql query")
   .option("--to <to>", "Destination file")
+  .option("--from <from>", "Source table, sub-select or sql file")
+  .option("--where <where>", "Query criteria")
   .option("-f, --force [true|false]", "Non-interactive, default=false", function(v) {return v !== "false"}, false)
   .option("-d, --debug [true|false]", "Show debug messages, default=false", function(v) {return v !== "false"}, false)
   .parse(process.argv);
