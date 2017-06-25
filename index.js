803dcaed3ac3777adcda0a0fa6859739933f3480
zander <zand3rs@gmail.com>
2017-06-25 10:45:40 +0800

    support for s3 as destination file

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var run = require("./lib/run");
 var program = require("commander").command(package.name);
 var args = process.argv.slice(2);
 
+var localDir = "/tmp/" + package.name;
+
 //==============================================================================
 
 program
@@ -16,6 +18,9 @@ program
   .option("--to <to>", "Destination file")
   .option("--from <from>", "Source table, subquery or sql file")
   .option("--where <where>", "Query criteria")
+  .option("--local-dir <local_dir>", "Local directory where remote files are synced, default=" + localDir, localDir)
+  .option("--access-key <access_key>", "AWS access key")
+  .option("--secret-key <secret_key>", "AWS secret key")
   .option("-f, --force [true|false]", "Non-interactive, default=false", function(v) {return v !== "false"}, false)
   .option("-d, --debug [true|false]", "Show debug messages, default=false", function(v) {return v !== "false"}, false)
   .parse(process.argv);
